42c834a0d199a50fa5c4090d181a0068025024d2
Win Le <nguyenwinle@gmail.com>
2018-07-15 17:43:23 -0700

    add firebase to js

--- app.js
+++ app.js
@@ -1,4 +1,16 @@
 $(document).ready(function() {
+    
+  // Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyBwTi2gj-BtvsllGeMVDZWk9AxN8Jcy8gs",
+    authDomain: "my-first-db-project-5c4ec.firebaseapp.com",
+    databaseURL: "https://my-first-db-project-5c4ec.firebaseio.com",
+    projectId: "my-first-db-project-5c4ec",
+    storageBucket: "",
+    messagingSenderId: "314678669232"
+  };
 
+  firebase.initializeApp(config);
+  var database = firebase.database();
     
 });
